ce62e4e431a5f1220f8b60e493b5d7ff246d2efc
mohammads19849 <mohammads19849@gmail.com>
2017-05-19 17:02:34 +0430

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by 09107110225 on 2/29/1396 AP.
+//  Copyright © 1396 Mohammad_Salimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PhotoramaTests/PhotoramaTests.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoramaTests.swift
+//  PhotoramaTests
+//
+//  Created by 09107110225 on 2/29/1396 AP.
+//  Copyright © 1396 Mohammad_Salimi. All rights reserved.
+//
+
+import XCTest
+@testable import Photorama
+
+class PhotoramaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
