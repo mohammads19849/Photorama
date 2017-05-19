25a64db1ec5be2ec9488d36eaeb7a62107617094
mohammads19849 <mohammads19849@gmail.com>
2017-05-19 17:11:04 +0430

    Create A Class class name is PhotoViewController

---
+++ Photorama/PhotoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoViewController.swift
+//  Photorama
+//
+//  Created by 09107110225 on 2/29/1396 AP.
+//  Copyright © 1396 Mohammad_Salimi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController:UIViewController{
+    
+    @IBOutlet var imageView : UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
